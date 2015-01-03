e58d119a87d3f1169dee071336b88f6e98dac883
unknown <dumm_heit@xyz.de>
2015-01-03 19:00:14 +0100

    sssssddddddd

--- bitcrystal.all.linux.make.sh
+++ bitcrystal.all.linux.make.sh
@@ -13,7 +13,6 @@ if [ $WHOAMI_ROOT -ne 0 ]; then
         echo "Are you running this script under root?"
         exit 0;
 fi
-
 $PREFIX cp my.leveldb.Makefile src/leveldb/Makefile
 $PREFIX rm src/leveldb/libleveldb.a
 $PREFIX make -f src/leveldb/Makefile -j4
@@ -29,16 +28,17 @@ else
         $PREFIX apt-get install qt4-qmake libqt4-dev build-essential libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev libminiupnpc-dev libgmp3-dev libmpfr-dev
 fi
 
-#$PREFIX apt-get install build-essential
-#$PREFIX apt-get install libssl-dev
-#$PREFIX apt-get install libboost-all-dev
-#$PREFIX apt-get install libdb4.8-dev
-#$PREFIX apt-get install libdb4.8++-dev
 if [[ $CLEAN_PROJECT == "true" ]]; then
 	$PREFIX rm src/obj/*.o
 	$PREFIX rm build_linux_objects/*.o
 fi
+$PREFIX apt-get install build-essential
+$PREFIX apt-get install libssl-dev
+$PREFIX apt-get install libboost-all-dev
+$PREFIX apt-get install libdb4.8-dev
+$PREFIX apt-get install libdb4.8++-dev
 $PREFIX apt-get install libminiupnpc-dev
+
 $PREFIX cp ./my.makefile.linux src
 $PREFIX cp ./build/build.h src
 $PREFIX cp ./build_deps/deps/qrencode-3.4.3/qrencode.h src/qt
