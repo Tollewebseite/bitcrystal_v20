068a17cc263357569fbaff58a6a9113153de9f5c
unknown <dumm_heit@xyz.de>
2015-02-24 18:03:01 +0100

    update16

---
+++ bitcrystal.ubuntu.install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+OS="UBUNTU"
+OS_VERSION="7.0"
+PREFIX=""
+if [[ $OS == "UBUNTU" ]]; then
+        PREFIX="sudo"
+fi
+
+WHOAMI_ROOT=$($PREFIX id -u);
+if [ $WHOAMI_ROOT -ne 0 ]; then
+        echo "Are you running this script under root?"
+        exit 0;
+fi
+
+$PREFIX echo bitcrystal is installing...
+$PREFIX cd ~
+$PREFIX rm -r bitcrystal_v20 1> /dev/null 2> /dev/null
+$PREFIX apt-get install git
+$PREFIX git clone https://github.com/bitcrystal/bitcrystal_v20 -b update14
+$PREFIX cd bitcrystal_v20
+$PREFIX chmod 755 ./ -R
+$PREFIX ./bitcrystal.ubuntu.all.linux.make.sh
+$PREFIX rm /usr/bin/bitcrystald 1> /dev/null 2> /dev/null
+$PREFIX rm /usr/bin/bitcrystal-qt 1> /dev/null 2> /dev/null
+$PREFIX cp src/bitcrystald /usr/bin
+$PREFIX cp ./bitcrystal-qt /usr/bin
+$PREFIX chmod 755 /usr/bin/bitcrystald
+$PREFIX chmod 755 /usr/bin/bitcrystal-qt
+$PREFIX chown $(whoami):$(whoami) /usr/bin/bitcrystald
+$PREFIX chown $(whoami):$(whoami) /usr/bin/bitcrystal-qt
+$PREFIX echo bitcrystal installed
