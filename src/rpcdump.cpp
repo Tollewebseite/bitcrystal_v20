3fa75b6f6fa40304eb980931e88709fb1b7d6729
unknown <dumm_heit@xyz.de>
2015-04-12 07:35:13 +0200

    dxxx

--- src/rpcdump.cpp
+++ src/rpcdump.cpp
@@ -1848,7 +1848,6 @@ Value getbalancefromtxids_multisigex(const Array& params, bool fHelp)
 	bool allok=getbalancefromtxids(account, balance, minconfirmations, txids);
 	if(!allok||balance<=0)
 	{
-		return minconfirmations;
 		return 0;
 	}
 	return balance;
